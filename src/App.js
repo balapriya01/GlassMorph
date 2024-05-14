c29d8285103904dacdd5ae4fa01e0a4fe370513a
Baswal <balapriya1801@gmail.com>
2024-05-14 19:03:11 +0530

    glassMorph

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { CiCreditCard1 } from "react-icons/ci";
 
-function App() {
+// Sapna pura hua, Always wanted to make this glass Morph since Grad 1st year.
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="sub-con">
+          <div className="circle1"></div>
+          <div className="card">
+            <div className="visa"> VISA</div>
+            <div className="icon">
+              <CiCreditCard1 className="icon" />
+            </div>
+            <div className="num">1234 5678 9123 2001</div>
+            <div className="text-con">
+              <p className="text1">
+                EUGENE FITZHERBERT &nbsp; &nbsp; &nbsp; VALID PERIOD 12/25
+              </p>
+            </div>
+          </div>
+          <div className="circle2"></div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
